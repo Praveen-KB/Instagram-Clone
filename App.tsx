1f32dec00b77b8b55436e8f4520ca53dc5a6204f
Praveen-KB <kbpraveen1999@gmail.com>
2023-06-28 00:51:44 +0530

    Praveen 28/06/2023 Initial Commit

---
+++ App.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import {
+  Dimensions,
+  SafeAreaView,
+
+  StyleSheet,
+  Text,
+
+  View,
+} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+
+
+      <View style={styles.sectionContainer}>
+        <Text>
+          Hi Lets Start the App Here
+        </Text>
+      </View>
+
+
+    </SafeAreaView>
+  );
+}
+
+const { width, height } = Dimensions.get("window")
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    height,
+    width,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
+
+export default App;
